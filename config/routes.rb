34fb4f6f8482918fd3cc56b37bec30076c0558c8
Christopher <ChrisKnight.mail@gmail.com>
2013-05-28 15:55:24 -0600

    dling xcode to update to rails 2. but working on local. trying to get up heroku before i move on

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Hactavism::Application.routes.draw do
   resources :links
 
+  root :to => "links#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
