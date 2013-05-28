bf50e0c9da3a7ff003b6eee8895db19976869e8b
Christopher <ChrisKnight.mail@gmail.com>
2013-05-28 13:52:14 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hactavism::Application.config.secret_key_base = '63d5d4ef636b49244284a1cc4f6b7d51df3e0708178e9e30dc7d12416aff07c9350853de158c3bc6487c2e27401ad28ce98764e057e54859967c46817b96d51d'
