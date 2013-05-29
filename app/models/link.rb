b25e7c9f6f1f1bf1bb82831c2083a7316467a817
Christopher <ChrisKnight.mail@gmail.com>
2013-05-29 13:52:43 -0600

    got the youtube regexp working

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,14 @@
 class Link < ActiveRecord::Base
+
+  def youtube_embed(youtube_url)
+    if youtube_url[/yout\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+
+    %Q{<iframe title="youTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  end
+
 end
