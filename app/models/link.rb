e4528ce7e9c1042e024b5071eceee27f11bd0250
Christopher <ChrisKnight.mail@gmail.com>
2013-05-29 14:23:24 -0600

    GOT VIMEO WORKING I AM A WIZARD

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,8 +7,23 @@ class Link < ActiveRecord::Base
       youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
       youtube_id = $5
     end
-
     %Q{<iframe title="youTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  end
+
+
+  def vimeo_embed(vimeo_url)
+    vimeo_id = vimeo_url[-8..-1]
+
+    %Q{<iframe id="moog" src="http://player.vimeo.com/video/#{ vimeo_id }" width="640" height="390" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>} 
+  end
+
+
+  def embed(url)
+    if url.include?("youtube")
+      youtube_embed(url)
+    elsif url.include?("vimeo")
+      vimeo_embed(url)
+    end
   end
 
 end
