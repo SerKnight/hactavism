7aa44572baff00fe53e278adb51d4993303d58bd
Christopher <ChrisKnight.mail@gmail.com>
2013-06-03 13:56:33 -0600

    css still gotta deal with the world pictue

--- lib/volunteer_match.rb
+++ lib/volunteer_match.rb
@@ -1,34 +1,34 @@
-# class VolunteerMatch
+class VolunteerMatch
   
-#   attr_reader :conn
+  attr_reader :conn
 
-#   def initialize
-#     @conn = connect
-#   end
+  def initialize
+    @conn = connect
+  end
 
-#   def connect 
-#     conn = Faraday.new(:url => 'http://www.volunteermatch.org/' ) do |faraday|
-#       faraday.request  :url_encoded             # form-encode POST params
-#       faraday.response :logger                  # log requests to STDOUT
-#       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-#     end
-#   end
+  def connect 
+    conn = Faraday.new(:url => 'http://www.volunteermatch.org/' ) do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
 
-#   def get_charity(tags)
-#     response = conn.get "api/call?action=searchOpportunities&keywords:#{tags}.json"
-#     puts response.body
-#   end
-
-
-# end
+  def get_charity(tags)
+    response = conn.get "api/call?action=searchOpportunities&keywords:#{tags}.json"
+    puts response.body
+  end
 
 
-# # Account Name: SerKnight
-# # Account Key: 99e302651dfd547e6d23954aba5afe94
+end
 
-# # GET /api/call?action=helloWorld&query=... HTTP/1.1
-# # Host: www.volunteermatch.org
-# # Accept-Charset: UTF-8
-# # Content-Type: application/json
-# # Authorization: WSSE profile="UsernameToken"
-# # VolunteerMatch Public-Use API User Manual, 11/09/2010 Page 3
+
+# Account Name: SerKnight
+# Account Key: 99e302651dfd547e6d23954aba5afe94
+
+# GET /api/call?action=helloWorld&query=... HTTP/1.1
+# Host: www.volunteermatch.org
+# Accept-Charset: UTF-8
+# Content-Type: application/json
+# Authorization: WSSE profile="UsernameToken"
+# VolunteerMatch Public-Use API User Manual, 11/09/2010 Page 3
