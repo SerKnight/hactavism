b4a0871cf5a94a5eba6301459e63b44f8d9bb611
Christopher <ChrisKnight.mail@gmail.com>
2013-07-15 13:52:49 -0600

    precomped assets

--- spec/features/upvoting_link_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "upvoting videos" do 
-  context "when clicking the upvtoe button" do 
-    it "adds one point to the video's total" do 
-      visit '/links'
-    end
-  end
-end
