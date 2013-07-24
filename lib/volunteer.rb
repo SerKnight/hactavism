4e7c7b1bb1e8804593fdd08f89a8da0f791694af
Christopher <ChrisKnight.mail@gmail.com>
2013-07-24 13:42:04 -0600

    volunteer api integrated

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,14 +1,15 @@
 class Volunteer
 
-	attr_reader :organization_name, :url
+	attr_reader :organization_name, :url, :volunteer_title
 
 	def initialize(opp)
 		@organization_name = opp["sponsoringOrganizationName"]
+		@volunteer_title = opp["title"]
 		@url = opp["xml_url"]
 	end
 
 	def self.opportunities(tags)
-		response = Faraday.get "http://www.allforgood.org/api/volopps?key=hacktiv13&q=#{tags}&num=3&output=json"
+		response = Faraday.get "http://www.allforgood.org/api/volopps?key=#{ENV['ALLFORGOODAPIKEY']}&q=#{tags}&num=3&output=json"
 		body = JSON.parse(response.body)
 		items = body["items"]
 		vol_ops = items[0..2]
--- spec/features/volunteer_api_spec.rb
+++ spec/features/volunteer_api_spec.rb
@@ -2,11 +2,12 @@ require "spec_helper"
 
 describe "when querying the all for good API" do 
 
-	let(:link) {Link.create(content: "http://www.youtube.com/watch?v=RkGf1iLERx8", tags: ["travel", "timor", "health"], description: "timor travel video")}
+	let(:link) {Link.create(content: "http://www.youtube.com/watch?v=RkGf1iLERx8", tags: "children, poverty", description: "timor travel video")}
 
 	context "on a video show page" do 
 		it "should query the api with the tags" do 
-			link.volunteer_option.count.should eq 3
+			tags = "children"
+			Volunteer.opportunities(tags).count.should eq 3
 		end
 	end
 end
